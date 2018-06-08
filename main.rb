b7da567cc031e86c8d770ede4ab4251d77b286a7
Kirby <jkirbycampbell@gmail.com>
2018-06-08 11:49:59 -0700

    Fixed Required paths

--- main.rb
+++ main.rb
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 require_relative './lib/activities/bike.rb'
 require_relative './lib/activities/hike.rb'
 require_relative './lib/activities/skiing.rb'
 require_relative './lib/cli.rb'
 require_relative './lib/input.rb'
-=======
-require_relative '../pdx-outdoor-master/lib/activities/bike.rb'
-require_relative '../pdx-outdoor-master//lib/activities/hike.rb'
-require_relative '../pdx-outdoor-master//lib/activities/skiing.rb'
-require_relative '../pdx-outdoor-master//lib/cli.rb'
-require_relative '../pdx-outdoor-master//lib/input.rb'
->>>>>>> 86cac6549f0ef13f2501cfc6c493c249592c0c79
 
 class Main
   include Cli
