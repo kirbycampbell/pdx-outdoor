155e184779cb8d6f090d5ef064faea78c96373a8
Kirby <jkirbycampbell@gmail.com>
2018-06-18 11:52:51 -0700

    Updated structure

--- bin/run.rb
+++ bin/run.rb
@@ -1,8 +1,4 @@
-require_relative './lib/activities/bike.rb'
-require_relative './lib/activities/hike.rb'
-require_relative './lib/activities/skiing.rb'
-require_relative './lib/cli.rb'
-require_relative './lib/input.rb'
+
 require_relative '../config/environment'
 
 class Main
--- config/environment.rb
+++ config/environment.rb
@@ -1,2 +1,7 @@
 require 'bundler/setup'
+require_relative '../lib/activities/bike.rb'
+require_relative '../lib/activities/hike.rb'
+require_relative '../lib/activities/skiing.rb'
+require_relative '../lib/cli.rb'
+require_relative '../lib/input.rb'
 Bundler.require(:default, :development)
